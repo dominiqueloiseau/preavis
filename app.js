fba31b22934a38d4ad687f78591737460a888780
domi7777 <domi7777@hotmail.com>
2015-05-29 15:04:43 +0000

    first steps

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
+var app = angular.module('noticeApp', ['ng-bootstrap-datepicker']);
 
-var app = angular.module('noticeApp', []);
+app.controller('NoticeCtrl', function($scope) {
+    $scope.dt = new Date();
 
-app.controller('NoticeCtrl', function($scope){
-     
+    $scope.open = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.opened = true;
+    };
+    $scope.datepickerOptions = {
+        format: 'dd/mm/yyyy',
+        language: 'fr',
+        autoclose: true,
+        weekStart: 0
+    }
+
+    $scope.date = new Date();
+
 });
