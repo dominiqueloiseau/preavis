503e165813ad84e8c718e562b7542a5904feafeb
domi7777 <domi7777@hotmail.com>
2015-06-06 10:01:14 +0000

    datepicker working 50%

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # preavis
 simple notice calculator
+
+TODO:
+- jasmine tests
+- travis integration 
+- travis status
+- preavis/démission tabs
+- work in progress
+- calendar directive
+- interval directive
+- localstorage
+- analytics
+- translation
--- app.js
+++ app.js
@@ -3,12 +3,7 @@ var app = angular.module('noticeApp', ['ng-bootstrap-datepicker']);
 app.controller('NoticeCtrl', function($scope) {
     $scope.dt = new Date();
 
-    $scope.open = function($event) {
-        $event.preventDefault();
-        $event.stopPropagation();
 
-        $scope.opened = true;
-    };
     $scope.datepickerOptions = {
         format: 'dd/mm/yyyy',
         language: 'fr',
@@ -16,6 +11,30 @@ app.controller('NoticeCtrl', function($scope) {
         weekStart: 0
     }
 
-    $scope.date = new Date();
+    $scope.dateStart = moment().format("DD/MM/YYYY");
+    $scope.dateEnd = moment().add(1, 'years').format("DD/MM/YYYY");
+
+    $scope.calculateNotice = function(newValue, oldValue /*for log only */){
+        var momentEnd = moment($scope.dateStart, "DD/MM/YYYY");
+
+        years = momentStart.diff(momentEnd, 'years');
+        momentStart.subtract(years, 'years');
+
+        months = momentStart.diff(momentEnd, 'months');
+        momentStart.subtract(months, 'months');
+
+        weeks = momentStart.diff(momentEnd, 'weeks');
+        momentStart.subtract(weeks, 'weeks');
+
+        days = momentStart.diff(momentEnd, 'days');
+
+        $scope.elapsedTime = (years ? years + " année(s) " : "") +
+            (months ? months + " mois" : "") +
+            (weeks ? weeks + " semaine(s)" : "") +
+            (days ? days + " jour(s)" : "");
+    }
+
+    $scope.$watch('dateStart', $scope.calculateNotice);
+    $scope.$watch('dateEnd', $scope.calculateNotice);
 
 });
