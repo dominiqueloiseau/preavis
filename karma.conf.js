0b105b379d7e0af12cae3ece6a8da50179246705
dominiqueloiseau <d.loiseau@csbconsulting.com>
2015-06-11 14:58:58 +0200

    trying to make it work on travis

--- karma.conf.js
+++ karma.conf.js
@@ -3,75 +3,45 @@
 
 module.exports = function(config) {
   config.set({
-
     plugins : ['karma-jasmine', 'karma-phantomjs-launcher'],
-
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
-
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
-
-
     // list of files / patterns to load in the browser
     files: [
       'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.0/angular.min.js',
       'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.3/moment.min.js',
       'src/*.js'
     ],
-
-
     // list of files to exclude
-    exclude: [
-      
-    ],
-
-
+    exclude: [],
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
-
-
+    preprocessors: {},
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
-
-
     // web server port
-    /*port: 9876,*/
-
-
+    port: 9876,
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
+    logLevel: config.LOG_DEBUG,
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
-    
     captureTimeout: 10000,
-
-
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['PhantomJS'],
-
-
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: true,
-    
-    hostname : process.env.IP,
-    port : process.env.PORT,
-    runnerPort : 0
-    
+    //hostname : process.env.IP,
+    //port : process.env.PORT,
+    //runnerPort : 0
   });
 };
