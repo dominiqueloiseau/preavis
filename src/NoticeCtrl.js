7efc3a102a4d49a45575d7fcd786d707ad7349de
domi7777 <domi7777@hotmail.com>
2015-06-11 15:07:12 +0000

    elapsed time test coverage

--- src/NoticeCtrl.js
+++ src/NoticeCtrl.js
@@ -1,38 +1,41 @@
 'use strict'
 
-var app = angular.module('noticeApp',  ['mgcrea.ngStrap'/*datePicker*/]);
-app.controller('NoticeCtrl', NoticeCtrl);
+var app = angular.module('noticeApp', ['mgcrea.ngStrap' /*datePicker*/ ]);
+app.controller('NoticeCtrl', window.NoticeCtrl);
 
-var NoticeCtrl = function($scope) {
+window.calculateElapsedTime = function(dateStart, dateEnd) {
+    var momentStart = moment(dateStart);
+    var momentEnd = moment(dateEnd);
+
+    var years, months, weeks, days;
+
+    years = momentEnd.diff(momentStart, 'years');
+    momentEnd.subtract(years, 'years');
+
+    months = momentEnd.diff(momentStart, 'months');
+    momentEnd.subtract(months, 'months');
+
+    weeks = momentEnd.diff(momentStart, 'weeks');
+    momentEnd.subtract(weeks, 'weeks');
+
+    days = momentEnd.diff(momentStart, 'days');
+
+    return (years ? years + " année(s) " : "") +
+        (months ? months + " mois " : "") +
+        (weeks ? weeks + " semaine(s) " : "") +
+        (days ? days + " jour(s) " : "");
+}
+
+window.NoticeCtrl = function($scope) {
 
     $scope.dateStart = moment().toDate();
     $scope.dateEnd = moment().add(1, 'days').toDate();
 
-    $scope.calculateNotice = function(newValue, oldValue /*for log only */){
-        var momentStart = moment($scope.dateStart);
-        var momentEnd = moment($scope.dateEnd);
-        
-        var years, months, weeks, days;
-        
-        years = momentEnd.diff(momentStart, 'years');
-        momentEnd.subtract(years, 'years');
-
-        months = momentEnd.diff(momentStart, 'months');
-        momentEnd.subtract(months, 'months');
-
-        weeks = momentEnd.diff(momentStart, 'weeks');
-        momentEnd.subtract(weeks, 'weeks');
-
-        days = momentEnd.diff(momentStart, 'days');
-
-        $scope.elapsedTime = (years ? years + " année(s) " : "") +
-            (months ? months + " mois " : "") +
-            (weeks ? weeks + " semaine(s) " : "") +
-            (days ? days + " jour(s) " : "");
+    $scope.calculateNotice = function(newValue, oldValue /*for log only */ ) {
+        $scope.elapsedTime = window.calculateElapsedTime($scope.dateStart, $scope.dateEnd);
     }
 
     $scope.$watch('dateStart', $scope.calculateNotice);
     $scope.$watch('dateEnd', $scope.calculateNotice);
 
 };
-
--- src/NoticeCtrlTest.js
+++ src/NoticeCtrlTest.js
@@ -1,26 +1,103 @@
-describe("NoticeCtrl", function() {
-  var newScope = function() {
-    return {
-      $watch: function() {}
-    }
-  };
-  var scope;
+'use strict'
+describe("NoticeCtrl > ", function() {
 
-  it("should init ctrl", function() {
-    new NoticeCtrl(newScope());
-  });
+    var newScope = function() {
+        return {
+            $watch: function() {}
+        }
+    };
+    var scope;
 
-  beforeEach(function() {
-    scope = newScope();
-    new NoticeCtrl(scope);
-  });
+    it("should init ctrl", function() {
+        new NoticeCtrl(newScope());
+    });
 
-  it("should calculate 1 day by default", function() {
-    // When
-    scope.calculateNotice();
-    
-    // Then
-    expect(scope.elapsedTime).toBe("1 jour(s) ");
-  });
-  
+    beforeEach(function() {
+        scope = newScope();
+        new NoticeCtrl(scope);
+    });
+
+    describe("Elapsed time calculation > ", function() {
+
+        it("should calculate 1 day by default", function() {
+            // When
+            scope.calculateNotice();
+
+            // Then
+            expect(scope.elapsedTime).toBe("1 jour(s) ");
+        });
+
+        it("should calculate 2 days ", function() {
+            // Given
+            var dateStart = new Date(2015, 5 /* == June!*/ , 9);
+            var dateEnd = new Date(2015, 5, 11);
+
+            // Then
+            expect(calculateElapsedTime(dateStart, dateEnd)).toBe("2 jour(s) ");
+        });
+
+        it("should calculate 1 week ", function() {
+            // Given
+            var dateStart = new Date(2015, 5, 9);
+            var dateEnd = new Date(2015, 5, 16);
+
+            // Then
+            expect(calculateElapsedTime(dateStart, dateEnd)).toBe("1 semaine(s) ");
+        });
+
+        it("should calculate 4 weeks when between 31th of May and 28 of June", function() {
+            // Given
+            var dateStart = new Date(2015, 4, 31);
+            var dateEnd = new Date(2015, 5, 28);
+
+            // Then
+            expect(calculateElapsedTime(dateStart, dateEnd)).toBe("4 semaine(s) ");
+        });
+
+        it("should calculate 4 weeks and 1 day when between 31th of May and 29 of June", function() {
+            // Given
+            var dateStart = new Date(2015, 4, 31);
+            var dateEnd = new Date(2015, 5, 29);
+
+            // Then
+            expect(calculateElapsedTime(dateStart, dateEnd)).toBe("4 semaine(s) 1 jour(s) ");
+        });
+
+        it("should calculate 1 month when between 1st of May and 1st of June", function() {
+            // Given
+            var dateStart = new Date(2015, 4, 1);
+            var dateEnd = new Date(2015, 5, 1);
+
+            // Then
+            expect(calculateElapsedTime(dateStart, dateEnd)).toBe("1 mois ");
+        });
+
+        it("should calculate 1 month 2 weeks 3 days", function() {
+            // Given
+            var dateStart = new Date(2015, 4, 1);
+            var dateEnd = new Date(2015, 5, 18);
+
+            // Then
+            expect(calculateElapsedTime(dateStart, dateEnd)).toBe("1 mois 2 semaine(s) 3 jour(s) ");
+        });
+
+        it("should calculate 1 year", function() {
+            // Given
+            var dateStart = new Date(2015, 5, 1);
+            var dateEnd = new Date(2016, 5, 1);
+
+            // Then
+            expect(calculateElapsedTime(dateStart, dateEnd)).toBe("1 année(s) ");
+        });
+
+        it("should calculate 5 years 4 months 3 weeks and 2 days", function() {
+            // Given
+            var dateStart = new Date(2010, 0, 1);
+            var dateEnd = new Date(2015, 4, 24);
+
+            // Then
+            expect(calculateElapsedTime(dateStart, dateEnd)).toBe("5 année(s) 4 mois 3 semaine(s) 2 jour(s) ");
+        });
+    });
+
 });
